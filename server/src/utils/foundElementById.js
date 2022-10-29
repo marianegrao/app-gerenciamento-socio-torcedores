66624d1f57763cd2e26240753b75f2566ffad3d0
Maria <claranegrao11@gmail.com>
2022-10-29 10:04:16 -0300

    Função auxiliar para encontrar elementos por id: nome de variável corrigida

--- server/src/utils/foundElementById.js
+++ server/src/utils/foundElementById.js
@@ -2,15 +2,15 @@ const knex = require("../connection");
 const foundElementById = async (elemtents, id) => {
 	const response = { error: false, message: "", data: "", status: 200 };
 	try {
-		const elmentFound = await knex(elemtents).where({ id }).first();
+		const elementFound = await knex(elemtents).where({ id }).first();
 
-		if (!elmentFound) {
+		if (!elementFound) {
 			response.error = true;
 			response.message = "Não foi encontrado elemento com o id informado.";
 			response.status = 404;
 		}
 
-		response.data = elmentFound;
+		response.data = elementFound;
 		return response;
 	} catch (error) {
 		response.error = true;
